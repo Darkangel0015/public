9835d7d495788e36f13b63b2e56d7de9d195af5a
Simon Carl Ferido <simoncarlferido@gmail.com>
2023-02-03 22:25:29 +0800

    git pull -u origin main

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,21 +53,3 @@ window.addEventListener("scroll", function () {
   }
 
 })
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function searchText() {
-  var input = document.getElementById("search").value;
-  var elements = document.getElementsByTagName("p");
-  for (var i = 0; i < elements.length; i++) {
-    if (elements[i].textContent.indexOf(input) >= 0) {
-      elements[i].style.backgroundColor = "yellow";
-    }
-  }
-}
